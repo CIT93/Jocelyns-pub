4933e232482b504c540f791451c20ab51fd4bf21
Jocelynnn11 <jocelynsandovalr11@gmail.com>
2024-08-19 13:04:20 -0700

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!')
+// How to Calculate Your Carbon Footprint - https://www.wikihow.com/Calculate-Your-Carbon-Footprint
+// Method 1
+
+// 1. Count the members of your household.
+    const myHouseMembers = 8;
+// 2. Consider the size of your home
+    const myHouseSize = 7;
+// 3. Evaluate your food choices
+    const myFoodChoice = 16;
+// 4. Examine your water consumption
+    const myWaterConsumption = 2;
+// 5. Determine how many household purchases you make each year.
+    const myHousePurchases = 6;
+// 6. Consider how much waste you produce
+    const myHouseWaste = 40;
+// 7. Identify the amount of waste that you recycle
+    const myHouseRecycle = 16;
+// 8. Tally up your annual transportation scores.
+    const myTransportation = 16;
+// 9. Add up your points
+    const cfpTotal = myHouseMembers + myHouseSize + myFoodChoice + myWaterConsumption + myHousePurchases + myHouseWaste + myHouseRecycle + myTransportation
+// 10. Write JS to update the rendered html (index.html) with total
+console.log("my Carbon Footprint is"); // 111

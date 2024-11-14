b08490b38dee2f489f9ba152cfa132a7fa7a5427
Jocelynnn11 <jocelynsandovalr11@gmail.com>
2024-11-14 15:45:15 -0800

    second attempt

--- w14/main.js
+++ w14/main.js
@@ -2,23 +2,27 @@ const output = document.getElementById("output");
 
 const url = "https://jsonplaceholder.typicode.com/posts";
 
+function renderPosts(data) {
+  data.forEach((post) => {
+    const postDiv = document.createElement("div");
+    const title = document.createElement("h3");
+    title.textContent = `Title: ${post.title}`;
+    const body = document.createElement("p");
+    body.textContent = `Body: ${post.body}`;
+
+    postDiv.appendChild(title);
+    postDiv.appendChild(body);
+    output.appendChild(postDiv);
+  });
+}
+
 async function getData() {
   try {
     const response = await fetch(url);
     if (response.status === 200) {
       const data = await response.json();
       console.log("Success", data);
-      data.forEach((post) => {
-        const postDiv = document.createElement("div");
-        const title = document.createElement("h3");
-        title.textContent = `Title: ${post.body}`;
-        const body = document.createElement("p");
-        body.textContent = `Body: ${post.body}`;
-
-        postDiv.appendChild(title);
-        postDiv.appendChild(body);
-        output.appendChild(postDiv);
-      });
+      renderPosts(data);
     } else {
       console.log(`Server Error: ${response.status}`);
     }
@@ -26,4 +30,5 @@ async function getData() {
     console.log(`Error: ${error}`);
   }
 }
+
 getData();

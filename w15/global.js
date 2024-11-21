3c8471f2da43c9010555d7469949f925566afb71
Jocelynnn11 <jocelynsandovalr11@gmail.com>
2024-11-21 01:34:40 -0800

    solution

--- w15/global.js
+++ w15/global.js
@@ -3,5 +3,3 @@ export const TBL = document.getElementById("tab-data");
 export const FNAME = document.getElementById('firstName');
 export const LNAME = document.getElementById('lastName');
 export const SUBMIT = document.getElementById("submitError");
-
-
--- w15/render.js
+++ w15/render.js
@@ -1,15 +1,39 @@
-
-import { FORM,TBL } from "./global.js";
+import { FORM, TBL } from "./global.js";
 import { saveLS } from "./storage.js";
 
-const renderTblHeading = () =>{
+const calculateAvg = (data) => {
+  const reduceTotal = data.reduce((sum, ea) => sum + ea.total, 0);
+  const tableRef = document.getElementById("table-id");
+  let newRow = tableRef.insertRow(-1);
+  let newCell = newRow.insertCell(0);
+  let newCell_1 = newRow.insertCell(0);
+  let newCell_2 = newRow.insertCell(0);
+  let newCell_3 = newRow.insertCell(0);
+  let newCell_4 = newRow.insertCell(0);
+  let newLabl = document.createTextNode(`Average Footprint`);
+  let newText = document.createTextNode(
+    `${Math.floor(reduceTotal / data.length)}`
+  );
+  newCell_1.appendChild(newLabl);
+  newCell.appendChild(newText);
+};
+
+const renderTblHeading = () => {
   //TBL.innerHTML = "";
   const table = document.createElement("table");
+  table.setAttribute("id", "table-id");
   const thead = document.createElement("thead");
   const tr = document.createElement("tr");
-  const headingTextArr = ["Name","HouseHold","HouseSize","FoodChoice","Footprint", "Actions",];
+  const headingTextArr = [
+    "Name",
+    "HouseHold",
+    "HouseSize",
+    "FoodChoice",
+    "Footprint",
+    "Actions",
+  ];
 
-  headingTextArr.forEach(text => {
+  headingTextArr.forEach((text) => {
     const th = document.createElement("th");
     th.textContent = text;
     tr.appendChild(th);
@@ -18,16 +42,15 @@ const renderTblHeading = () =>{
   thead.appendChild(tr);
   table.appendChild(thead);
   return table;
-}
+};
 
-const onUpdate = (index,data) =>{
-  data.splice(index,1);
+const onUpdate = (index, data) => {
+  data.splice(index, 1);
   saveLS(data);
-  renderTbl(data);  
-}
+  renderTbl(data);
+};
 
-
-const renderTblBtn = (obj,index,data)=>{
+const renderTblBtn = (obj, index, data) => {
   const td = document.createElement("td");
   const btnEdit = document.createElement("button");
   const btnDel = document.createElement("button");
@@ -35,64 +58,47 @@ const renderTblBtn = (obj,index,data)=>{
   btnDel.textContent = "Del";
   td.appendChild(btnEdit);
   td.appendChild(btnDel);
-  btnDel.addEventListener('click',e =>{
-    onUpdate(index,data);
+  btnDel.addEventListener("click", (e) => {
+    onUpdate(index, data);
   });
 
-  btnEdit.addEventListener('click',e => {
-    FORM[1].value=obj.first;
-    FORM[2].value=obj.last;
-    FORM[3].value=obj.houseMembers;
-    FORM[4].value=obj.houseSize;
-    FORM[5].value=obj.foodChoice
-   onUpdate(index,data); 
-     
-});
-return td;
-}
+  btnEdit.addEventListener("click", (e) => {
+    FORM[1].value = obj.first;
+    FORM[2].value = obj.last;
+    FORM[3].value = obj.houseMembers;
+    FORM[4].value = obj.houseSize;
+    FORM[5].value = obj.foodChoice;
+    onUpdate(index, data);
+  });
+  return td;
+};
 const renderTblBody = (data) => {
   const tbody = document.createElement("tbody");
-  data.forEach((obj, index) =>{
+  data.forEach((obj, index) => {
     const tr = document.createElement("tr");
-    const keys = ["first","houseMembers","houseSize","foodChoice","total"]
-    keys. forEach(key =>{
-       const td = document.createElement("td");
-        td.textContent = obj [key];
-        tr.appendChild(td);
-  })
+    const keys = ["first", "houseMembers", "houseSize", "foodChoice", "total"];
+    keys.forEach((key) => {
+      const td = document.createElement("td");
+      td.textContent = obj[key];
+      tr.appendChild(td);
+    });
 
-   const td = renderTblBtn(obj, index, data);
+    const td = renderTblBtn(obj, index, data);
     tr.appendChild(td);
     tbody.appendChild(tr);
-    
   });
   return tbody;
-}
+};
 
-const renderTbl = data  => {
+const renderTbl = (data) => {
   TBL.innerHTML = "";
-  const totalFootprint = data.reduce((sum,obj) => sum + obj.total,0);
-  const averageFootprint = totalFootprint/ data.length;
-  if (data.length !==0){
+  if (data.length !== 0) {
     const table = renderTblHeading();
-  const tbody = renderTblBody(data);
-  table.appendChild(tbody);
- 
-  const tfoot = document.createElement("tfoot");
-  const tfootRow = document.createElement("tr");
-
-  const th = document.createElement("th");
-  th.textContent = "Average Footprint Score:";
-  tfootRow.appendChild(th);
-
-  const td = document.createElement("td");
-  td.textContent = averageFootprint;
-  tfoot.appendChild(tfootRow);
-  table.appendChild(tfoot);
-  TBL.appendChild(table);
-  } 
-   
+    const tbody = renderTblBody(data);
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+    calculateAvg(data);
   }
+};
 
-
-export { renderTbl};
+export { renderTbl };
